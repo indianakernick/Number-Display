e316eaa5f7f50d0f1f475d62a77c3b4c5fa07fef
Kerndog73 <kerndog73@gmail.com>
2017-07-29 19:09:46 +0930

    Use a different font
    I just noticed that the font is 5x8 and not 5x7 so some of the glyphs were being truncated. I found this font as some C code so I didn't have to process an image this time.

--- Number Display/5x7 dot matrix.cpp
+++ Number Display/5x7 dot matrix.cpp
@@ -7,3 +7,51 @@
 //
 
 #include "5x7 dot matrix.hpp"
+
+#include <cmath>
+
+using namespace Display::Dot5x7;
+
+constexpr float PI2 = 6.28318530718f;
+
+namespace {
+  void pushColor(std::vector<float> &colors, const Color color) {
+    colors.push_back(color.r);
+    colors.push_back(color.g);
+    colors.push_back(color.b);
+    colors.push_back(color.a);
+  }
+
+  void drawCircle(
+    Mesh &mesh,
+    const float x,
+    const float y,
+    const float radius,
+    const unsigned detail,
+    const Color color
+  ) {
+    const unsigned centerIndex = static_cast<unsigned>(mesh.colors.size());
+  
+    pushColor(mesh.colors, color);
+    mesh.verts.push_back(x);
+    mesh.verts.push_back(y);
+    
+    for (unsigned i = 0; i != detail; i++) {
+      //counter clockwise winding
+      mesh.indicies.push_back(static_cast<unsigned>(mesh.colors.size()));
+      pushColor(mesh.colors, color);
+      mesh.indicies.push_back(static_cast<unsigned>(mesh.colors.size()));
+      mesh.indicies.push_back(centerIndex);
+      const float angle = static_cast<float>(i) / detail * PI2;
+      mesh.verts.push_back(x + std::cos(angle) * radius);
+      mesh.verts.push_back(y + std::sin(angle) * radius);
+    }
+  }
+}
+
+Mesh Display::Dot5x7::circles(
+  const std::experimental::string_view str,
+  const CircleConfig config
+) {
+  
+}
